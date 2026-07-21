b9dc8d477070a140c268134d1fb28f12c73465f2
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-07-21 07:38:56 +0300

    Question02

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -11,6 +11,14 @@
             -Dynamic binding based on object -it occurs with method override and interface -it happens in run time-executes the method based on  object
              */
             #endregion
+            #region Q02
+            /*
+             -Method overloading is a static polymorphism -it happens at compile time -based on refrence-it's the Same method name but
+              Different parameter list 
+            -Method Overriding is a dynamic polymorphism -it happens at run time -based on object -it's require inheritance - using virtual and override 
+             The parent method must be virtual - the same method but different implementation  
+             */
+            #endregion
             #endregion
         }
     }
