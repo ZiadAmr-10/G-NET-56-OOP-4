dd9fd0fa141c4f948c4a7cb57a1937bf92a332e6
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-07-21 07:28:35 +0300

    Question01

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+            #region Q01
+            /*
+            -Static binding based on reference -it occurs with method overloading and method hiding -it happens in compile time - execute the method based on  reference
+            -Dynamic binding based on object -it occurs with method override and interface -it happens in run time-executes the method based on  object
+             */
+            #endregion
+            #endregion
         }
     }
 }
